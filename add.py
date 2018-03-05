233134ded99c0b181e283a171a1e1c1d223a18fd
Rahul14797 <rahul.s.shukla97@gmail.com>
2018-03-05 12:04:29 +0530

    First

---
+++ add.py
@@ -0,0 +1,5 @@
+a=input("Enter Value for A:")
+b=input("Enter Value for B:")
+sum=a+b
+print('the sum of {0} and {1} is {2}'.format(a,b,sum))
+
---
+++ student.py
@@ -0,0 +1,9 @@
+a=input("Enter the no of students:-")
+student_data=["student_roll no","student_name","student_class"]
+for i in (0:n)
+student_roll no=(input("Enter the Roll No:"))
+print student_roll no
+student_name=(input("Enter the Student Name:-"))
+print student_name
+student_class(input("Enter the student Class:-"))
+print student_class
